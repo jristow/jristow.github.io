3036f3b1e284e3bcd11ae26e64b25ae80b5257a6
jristow <jristow@gmail.com>
2019-10-15 06:56:05 -0500

    Updates

--- sw.js
+++ sw.js
@@ -26,7 +26,7 @@ workbox.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-39cb5496279e12d13857.js"
+    "url": "webpack-runtime-642635f083049b6eee50.js"
   },
   {
     "url": "styles.1025963f4f2ec7abbad4.css"
@@ -42,7 +42,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "a87f0986a5e25e328072deb9c726fe4c"
+    "revision": "07d3862f2525a68636a357060074f0b8"
   },
   {
     "url": "component---src-pages-404-js-997b167f761c4a745df9.js"
